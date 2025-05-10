09a0cda9a4041be418d1a9c89f99346ee3d133f7
binara shenal <kahadhugodakankanamge.com>
2025-05-10 21:29:59 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { About } from './pages/About'
+import { SignIn } from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import { Home } from './pages/Home'
+
 export default function App() {
   return (
-    <h1>
-      app
-    </h1>
-     
+    <BrowserRouter>
+       <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign_in" element={<SignIn />} />
+          <Route path="/sign_up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/prjects" element={<Projects />} />
+       </Routes>
+    
+    </BrowserRouter>
   )
 }
