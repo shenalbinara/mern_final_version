1dd0dd4e713eb1cf8dcae4a711e6e3d879a4fd06
binara shenal <kahadhugodakankanamge.com>
2025-05-11 17:06:24 +0530

    api signup route create

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user_route.js';
+import authRoures from './routes/auth.route.js';
+
 
 // Load environment variables from .env file
 dotenv.config();
@@ -20,6 +22,9 @@ mongoose.connect(process.env.MONGO)
 // Initialize Express
 const app = express();
 
+app.use(express.json());
+
+
   
 app.listen(3000, () => {
     console.log('server is running on port 3000')
@@ -27,3 +32,4 @@ app.listen(3000, () => {
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoures);
