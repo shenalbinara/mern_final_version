82ea4d489a5aea414a9acb111c3a4e7aa1250666
binara shenal <kahadhugodakankanamge.com>
2025-05-17 11:20:28 +0530

    create get postapi

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -13,10 +13,11 @@ export default router;
 
 // post.route.js
 import express from 'express';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', create); // Removed verifyToken middleware
+router.get('/getposts', getposts)
 
 export default router;
