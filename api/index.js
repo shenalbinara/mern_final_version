91c6829ec61ea410924a29bfdada3020252777cc
binara shenal <kahadhugodakankanamge.com>
2025-05-27 11:02:13 +0530

    Initial commit of final version

--- api/index.js
+++ api/index.js
@@ -1,65 +1,52 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user_route.js';
+import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-import postRoutes from'./routes/post.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
-import commentRoutes from './routes/comment.route.js'
 import path from 'path';
 
-
-
-// Load environment variables from .env file
 dotenv.config();
 
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log('Mongodb is connected ')
-
-})
-.catch((err) => {
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
-// Initialize Express
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-
-
-  
 app.listen(3000, () => {
-    console.log('server is running on port 3000')
-})
-
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes)
+app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message = err.message || 'internal srver error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
